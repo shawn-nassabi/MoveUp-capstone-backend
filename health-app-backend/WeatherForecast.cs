a17f5645e79f8ed91cf6d570da1d45708fa49c26
Shawn Nassabi <san7522@nyu.edu>
2024-10-29 02:14:30 +0400

    Initial project setup

---
+++ health-app-backend/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace health_app_backend;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
